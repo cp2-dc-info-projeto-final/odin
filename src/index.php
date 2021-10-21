733a509a0670cbd9e27a420e50f1423457ee2e95
Erick Silva de Lima <51960797+ErickSDL@users.noreply.github.com>
2021-10-21 16:25:48 -0300

    Home page, navbar responsiva.
    Essa é versão inicial da home page, fiz uma nav bar responsiva que se altera conforme a mudança da tela.  Lucas deve adicionar nessa semana ou no começo da semana que vêm o cadastro sobre essa home page e melhorar o design como adição de logo e mudança nas cores.

--- src/index.php
+++ src/index.php
@@ -1,8 +1,31 @@
-<html>
- <head>
-   <title>Hello World</title>
- </head>
- <body>
-   <?php echo "Hello World!";?>
- </body>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Odin</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+
+  <body>
+    <header>
+      <nav>
+        <a class="logo" href="/">Projeto Odin</a>
+        <div class="mobile-menu">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+        <ul class="nav-list">
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Perfil</a></li>
+          <li><a href="#">Postagens</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main></main>
+    <script src="mobile-navbar.js"></script>
+  </body>
 </html>
