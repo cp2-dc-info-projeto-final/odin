2c9d2485da3dd7f98d7ae0a2fc6c678198c911da
Lucas Fernando Miranda <90480715+Lucas-Fernando-0@users.noreply.github.com>
2021-10-27 16:33:50 -0300

    backend do login junto com front end do cadastro

--- src/index.php
+++ src/index.php
@@ -18,7 +18,37 @@
           <div class="line2"></div>
           <div class="line3"></div>
         </div>
-                
+      
+        <div class="content">      
+      <div id="login">
+        <form method="post" action=""> 
+          <h1>Login</h1> 
+          <p> 
+            <label for="nome_login">Seu nome</label>
+            <input id="nome_login" name="nome_login" required="required" type="text" placeholder="ex. contato@htmlecsspro.com"/>
+          </p>
+           
+          <p> 
+            <label for="email_login">Seu e-mail</label>
+            <input id="email_login" name="email_login" required="required" type="password" placeholder="ex. senha" /> 
+          </p>
+           
+          <p> 
+            <input type="checkbox" name="manterlogado" id="manterlogado" value="" /> 
+            <label for="manterlogado">Manter-me logado</label>
+          </p>
+           
+          <p> 
+            <input type="submit" value="Logar" /> 
+          </p>
+           
+          <p class="link">
+            Ainda não tem conta?
+            <a href="#paracadastro">Cadastre-se</a>
+          </p>
+        </form>
+      </div>
+        
         <ul class="nav-list">
           <li><a href="index.php">Entrar</a></li>
           <li><a href="cadastro.php">Cadastrar-se</a></li>
