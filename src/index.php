68eb295087dc5334c840fd9d15c1dda210ce47f3
Erick Silva de Lima <51960797+ErickSDL@users.noreply.github.com>
2021-11-13 23:02:17 -0300

    atualização navbar

--- src/index.php
+++ src/index.php
@@ -1,62 +1,99 @@
 <!DOCTYPE HTML>
 <html lang="pt-br">
-
   <?php 
     include "auth.inc";
+    include "mysqli_connect.inc";
   ?>
-
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://kit.fontawesome.com/c22820d043.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Odin</title>
-    <link rel="stylesheet" href="_css/style.css" />
+    <link rel="stylesheet" href="_css/stylenavbar.css" />
   </head>
 
   <body>
     <header>
-      <script src="_js/logout.js"></script>
-      <nav>
-        <a class="logo" href="index.php">Odin</a>
+      <nav class="navbar fixed-top navbar-expand-sm 
+      navbar-dark bg-dark">
 
-        <div class="mobile-menu">
-          <div class="line1"></div>
-          <div class="line2"></div>
-          <div class="line3"></div>
-          <div class="line4"></div>
+        <a  
+          href="#" 
+          class="navbar-brand mb-0 h1">
+            <img 
+            class="d-inline-block align-top" src="./assets/icon8.svg.jpeg" 
+            width="30" height="30"/>
+          Odin
+        </a>
+        <button 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav" 
+        aria-controls="navbarNav" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation" 
+        class="navbar-toggler"
+        >
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a href="#" class="nav-link active">
+                Perfil
+              </a>
+            </li>
+            <li class="nav-item active">
+              <a href="#" class="nav-link">
+                Amigos
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link 
+              dropdown-toggle" 
+              id="navbarDropdown" 
+              role="button" 
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              >
+                Opções
+              </a>
+              <ul class="dropdown-menu"
+              aria-labelledby="navbarDropdown">
+                <li><a href="editar.php" 
+                  class="dropdown-item">Editar Usuario</a></li>
+                <li><a href="#" 
+                  class="dropdown-item">Ajuda e Suporte</a></li>
+                <li><a href="#" 
+                  class="dropdown-item">Sair</a></li>
+
+              </ul>
+            </li>
+          </ul>          
         </div>
-
-      <div class="search-box">
-        <form action = "busca.php" method = "POST">
-          <input type = "hidden" name = "operacao" value = "buscar">
-          <input class="search-txt" type="text" name = "busca" placeholder="Pesquise Aqui">
+        <form class="d-flex">
+          <input type="text" class="form-control me-2">
+          <button 
+          type="submit" class="btn btn-primary custom-btn" style="color: white;">Procurar</button>
+            Procurar
+          </button>
         </form>
-      </div>
-
-        <ul class="nav-list">
-          <li><a href="#">Perfil</a></li>
-          <?php
-            if ($usuario["adm"] == 1){
-              echo "<li><a href='dashboard.php'>Dashboard</a></li>";
-            }
-          ?>
-          <li><a href="editar.php">Editar usuário</a></li>
-          <li><a onclick="confirmarSaida()">Sair</a></li>
-        </ul>
       </nav>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </header>
-    
+
     <main class="main">
       <script src="_js/mobile-navbar.js"></script>
-       
+
       <!---Formulario de Postagens-->
+      <li style="height: 50px;"></li>
       <div class="newPost">
           <div class="infoUser">
                 <div class="imgUser"></div>
                 <strong><?php echo $usuario["nome"] . " " . $usuario["sobrenome"]; ?></strong>
             </div>    
-
                 <form action="" class="formPost">
                     <textarea name="textarea" placeholder="Mostre seus produtos !!!"></textarea>
                     
@@ -66,7 +103,6 @@
                             <button class="btnFileForm"><img src="./assets/gif.svg" alt="Adicionar um gif"></button>
                             <button class="btnFileForm"><img src="./assets/video.svg" alt="Adicionar um video"></button>
                         </div>
-
                         <button type="submit" class="btnSubmitForm">Publicar</button>
                     </div>
                 </form>
@@ -82,22 +118,17 @@
               <p>14h</p>
             </div>
           </div>
-
         <p>
           Queria compartilhar o quão barato está o PS6 e a placa de vídeo 4090, estão por somente 10 reais aqui na Lucas'bazar 
           sim e essa é a ultima chance de você conseguir esses produtos por esses preços e na compra dos dois você ganha 
           um xbox series Z venha logo a Rua outubro melhor mês N 666
         </p>
-
         <div class="actionBtnPost">
           <button type="button" class="filesPost like"><img src="./assets/heart.svg" alt="Curtir">Curtir</button>
           <button type="button" class="filesPost comment"><img src="./assets/comment.svg" alt="Comentar">Comentar</button>
           <button type="button" class="filesPost share"><img src="./assets/share.svg" alt="Compartilhar">Compartilhar</button>
-          <?php
-            if ($usuario["adm"] == 1 /* || $usuario["id"] == $post["id_usuario"]*/){
-              echo '<button type="button" class="filesPost like">Excluir</button>';
-            }
-          ?>
+          <button type="button" class="filesPost like">Excluir</button>
+
         </div>
 
         </li>
@@ -111,13 +142,11 @@
               <p>14h</p>
             </div>
           </div>
-
         <p>
           Queria compartilhar o quão barato está o PS6 e a placa de vídeo 4090, estão por somente 10 reais aqui na Lucas'bazar 
           sim e essa é a ultima chance de você conseguir esses produtos por esses preços e na compra dos dois você ganha 
           um xbox series Z venha logo a Rua outubro melhor mês N 666
         </p>
-
         <div class="actionBtnPost">
           <button type="button" class="filesPost like"><img src="./assets/heart.svg" alt="Curtir">Curtir</button>
           <button type="button" class="filesPost comment"><img src="./assets/comment.svg" alt="Comentar">Comentar</button>
@@ -126,7 +155,6 @@
         </div>
 
         </li>
-
         <li class="post">
           <div class="infoUserPost">
             <div class="imgUserPost"></div>
@@ -136,13 +164,11 @@
               <p>14h</p>
             </div>
           </div>
-
         <p>
           Queria compartilhar o quão barato está o PS6 e a placa de vídeo 4090, estão por somente 10 reais aqui na Lucas'bazar 
           sim e essa é a ultima chance de você conseguir esses produtos por esses preços e na compra dos dois você ganha 
           um xbox series Z venha logo a Rua outubro melhor mês N 666
         </p>
-
         <div class="actionBtnPost">
           <button type="button" class="filesPost like"><img src="./assets/heart.svg" alt="Curtir">Curtir</button>
           <button type="button" class="filesPost comment"><img src="./assets/comment.svg" alt="Comentar">Comentar</button>
@@ -151,10 +177,7 @@
         </div>
 
         </li>
-
       </ul>
-
     </main>
-
   </body>
 </html>
