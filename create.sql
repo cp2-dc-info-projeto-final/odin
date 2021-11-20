28e2adc75e3bfc2a8c9373d40c75d2d879b5c5cf
ZzZerafin <marcos.gabriel.s.t2004@gmail.com>
2021-11-20 17:22:12 -0300

    Tabela SQL - Posts

--- create.sql
+++ create.sql
@@ -13,6 +13,15 @@ CREATE TABLE usuarios (
     adm tinyint(1) NOT NULL DEFAULT 0
 );
 
+CREATE TABLE posts (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuario int NOT NULL,
+    data_hora DATETIME NOT NULL,
+    texto varchar(400) NOT NULL,
+    midia varchar(200),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id);
+)
+
 -- Cadastre seu primeiro usuário e use esse comando para torná-lo administrador da rede social:
 
 UPDATE usuarios SET adm = 1 WHERE id = 1;
